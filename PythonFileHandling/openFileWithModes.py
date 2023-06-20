eeb862621842e7bfd5af00bc5e88561f77c3444a
buddha satta dutta roy <buddhasatta6@gmail.com>
2023-06-20 17:48:47 +0530

    The file contains code to read write and handle files with Python.

---
+++ PythonFileHandling/openFileWithModes.py
@@ -0,0 +1,14 @@
+file1=open("D:\GitHub\ClassOfPython\TestFile.txt","rt");
+
+#following is the command to read the file
+print(file1.read())
+
+#this is the command to read  line by  line together
+#this is the command to read  line by  line together
+
+print(file1.readline())
+print(file1.readline())
+
+print(file1.readline())
+
+print(file1.readline())
